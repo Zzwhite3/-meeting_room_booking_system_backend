6b9689bb5cc3f9519af895cf038c50e0c960b314
yindada <17771449436@163.com>
2025-11-16 12:14:18 +0800

    feat: 建立permisson,role,user的数据库表结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,28 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
     // 调用 TypeOrmModule 的 forRoot 方法，初始化根数据库连接配置
     TypeOrmModule.forRoot({
       type: 'mysql',                    // 指定数据库类型为 MySQL
-      host: 'localhost',                // 指定数据库主机地址为本地主机
+      host: '127.0.0.1',                // 指定数据库主机地址为本地主机
       port: 3306,                       // 指定数据库端口号为 3306（MySQL 默认端口）
       username: 'root',                 // 指定数据库用户名为 root
-      password: 'password',             // 指定数据库密码为 'password'
+      password: 'yindada',                     // 指定数据库密码为空（默认 MySQL 安装）
       database: 'meeting_room_booking_system', // 指定要连接的数据库名称
-      entities: [],                     // 指定数据库实体列表，当前为空数组
+      entities: [Role, Permission, User],                     // 指定数据库实体列表
       synchronize: true,                // 启用数据库同步功能，自动创建和更新表结构（生产环境不推荐）
       logging: true,                    // 启用数据库操作日志记录，输出所有 SQL 查询语句
       poolSize: 10,                     // 指定数据库连接池的大小为 10，限制最大连接数
       connectorPackage: 'mysql2',       // 指定使用 mysql2 作为 MySQL 连接器包
-      extra: {                          // 额外的数据库配置
-        authPlugin: 'sha256_password',  // 指定认证插件为 sha256_password（MySQL 8.0+ 默认认证方式）
-      },
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
