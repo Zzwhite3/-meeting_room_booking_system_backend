fad217a316ae1c49a4f1a66d51fe703c3adbabd9
yindada <17771449436@163.com>
2026-01-25 15:48:40 +0800

    feat: 开启数据格式拦截器，和日志打印拦截器

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   const configService = app.get(ConfigService);
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor());
   await app.listen(configService.get('nest_server_port') as string);
 
 }
